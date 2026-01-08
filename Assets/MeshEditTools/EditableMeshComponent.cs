0ba2eb9acbec4ced00eca07b2762ab5421214d9d
madfishOD <madfish.od@gmail.com>
2026-01-08 14:24:42 +0200

    Add mesh conversion and selection tools

--- Assets/MeshEditTools/EditableMeshComponent.cs
+++ Assets/MeshEditTools/EditableMeshComponent.cs
@@ -6,8 +6,10 @@ namespace MeshEditTools
     {
         [SerializeField] private EditableMesh editableMesh = new();
         [SerializeField] private MeshFilter targetMeshFilter;
+        [SerializeField] private MeshSelectionMode selectionMode = MeshSelectionMode.Vertex;
 
         public EditableMesh Mesh => editableMesh;
+        public MeshSelectionMode SelectionMode => selectionMode;
 
         public void BakeToUnityMesh()
         {
@@ -26,5 +28,28 @@ namespace MeshEditTools
             unityMesh.name = name + "_Baked";
             targetMeshFilter.sharedMesh = unityMesh;
         }
+
+        public void ConvertFromUnityMesh()
+        {
+            if (targetMeshFilter == null)
+            {
+                targetMeshFilter = GetComponent<MeshFilter>();
+            }
+
+            if (targetMeshFilter == null || targetMeshFilter.sharedMesh == null)
+            {
+                Debug.LogWarning("EditableMeshComponent requires a MeshFilter with a mesh to convert.", this);
+                return;
+            }
+
+            editableMesh = EditableMesh.FromUnityMesh(targetMeshFilter.sharedMesh);
+        }
+    }
+
+    public enum MeshSelectionMode
+    {
+        Vertex,
+        Edge,
+        Face
     }
 }
